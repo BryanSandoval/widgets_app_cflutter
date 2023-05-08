30159c7e4d4ff5960d71279f8602f653d69f96b7
Bryan Sandoval <bchas831@gmail.com>
2023-05-08 11:45:38 -0600

    Se actualiza el main para que vea el scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:widgets_app/config/theme/app_theme.dart';
+import 'package:widgets_app/presentation/screens/home/home_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,16 +11,7 @@ class MyApp extends StatelessWidget {
       title: 'Material App',
       debugShowCheckedModeBanner: false,
       theme: AppTheme(selectedColor: 6).getTheme(),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
+      home: const HomeScreen(),
     );
   }
 }
