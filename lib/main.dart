6087c86bd931d116d347b9970493bfd71559100c
Bryan Sandoval <bchas831@gmail.com>
2023-05-08 15:09:24 -0600

    go_router configurado 0805

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:widgets_app/config/theme/app_theme.dart';
+import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
+import 'package:widgets_app/presentation/screens/cards/cards_screen.dart';
 import 'package:widgets_app/presentation/screens/home/home_screen.dart';
 
-void main() => runApp(MyApp());
+import 'config/router/app_router.dart';
+
+void main() => runApp(
+      const MyApp(),
+    );
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routerConfig: appRouter,
       title: 'Material App',
       debugShowCheckedModeBanner: false,
       theme: AppTheme(selectedColor: 0).getTheme(),
-      home: const HomeScreen(),
     );
   }
 }
--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 import '../../../../config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -51,7 +53,9 @@ class _CustomListTile extends StatelessWidget {
       ),
       title: Text(menuItem.title),
       subtitle: Text(menuItem.subTitle),
-      onTap: () {},
+      onTap: () {
+        context.push(menuItem.link);
+      },
     );
   }
 }
